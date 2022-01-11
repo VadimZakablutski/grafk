007ebcd65e31639da701e1b82d99e85362b7924f
opilane <opilane@HP-CZC6297B9H>
2022-01-11 11:28:01 +0200

    j

--- grafk/grafk.py
+++ grafk/grafk.py
@@ -69,26 +69,48 @@ def kit():
     x1=np.arange(0,9,0.5)
     y1=(2/27)*x1*x1-3
     x2=np.arange(-10,0,0.5)
-    y2=0.04*x1*x1-3
+    y2=0.04*x2*x2-3
     x3=np.arange(-9,-3,0.5)
-    y3=(2/9)*(x+6)*x1*x1+1
+    y3=(2/9)*(x3+6)**2+1
     x4=np.arange(-3,9,0.5)
-    y4=(-1/12)*(x-3)*x1*x1+6
+    y4=(-1/12)*(x4-3)**2+6
     x5=np.arange(5,8.3,0.5)
-    y5=(1/9)*(x-5)*x1*x1+2
+    y5=(1/9)*(x5-5)**2+2
     x6=np.arange(5,8.5,0.5)
-    y6=(1/8)*(x-7)*x1*x1+1.5
+    y6=(1/8)*(x6-7)**2+1.5
     x7=np.arange(-13,-9,0.5)
-    y8=-0.75*(x+11)*x1*x1+6
+    y7=(-0.75)*(x7+11)**2+6
+    x8=np.arange(-15,-10,0.5)
+    y8=(-0.5)*(x8+13)**2+3
     x9=np.arange(-15,-10,0.5)
-    y9=-0.5(x+13)*x1*x1+3
-    x10=np.arange(-15,-10,0.5)
-    y10=1
-    x11=np.arange(3,4,0.5)
-    y11=3
+    y9=1
+    x10=np.arange(3,4,0.5)
+    y10=3
     fig = plt.figure()
-    plt.plot(x, y,'b:o', x0, y0,'g-d')
-    plt.title('Квадратное уравнение')
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
+    plt.title('Кит')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+def glases():
+    x1=np.arange(-9,-1,0.5)
+    y1=(-1/16)*(x1+5)**2+2
+    x2=np.arange(1,9,0.5)
+    y2=(-1/16)*(x2-5)**2+2
+    x3=np.arange(-9,-1,0.5)
+    y3=1/4*(x3+5)**2-3
+    x4=np.arange(1,9,0.5)
+    y4=1/4*(x4-5)**2-3
+    x5=np.arange(-9,-6,0.5)
+    y5=-(x5+7)**2+5
+    x6=np.arange(6,9,0.5)
+    y6=-(x6-7)**2+5
+    x7=np.arange(-1,1,0.5)
+    y7=(-0.5)*x7*x7+1.5
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title('Очки')
     plt.ylabel('y')
     plt.xlabel('x')
     plt.grid(True)
@@ -113,7 +135,7 @@ c.pack(side=LEFT)
 y=Label(aken,text="=0",font="Calibri 26", fg="green")
 y.pack(side=LEFT)
 Rd=Radiobutton(aken,text="Кит",font="Calibri 26", fg="green",command=kit)
-Rd2=Radiobutton(aken,text="Очки",font="Calibri 26", fg="green")
+Rd2=Radiobutton(aken,text="Очки",font="Calibri 26", fg="green",command=glases)
 Rd3=Radiobutton(aken,text="Зонтик",font="Calibri 26", fg="green")
 Rd4=Radiobutton(aken,text="Бабочка",font="Calibri 26", fg="green")
 Rd5=Radiobutton(aken,text="Лягушка",font="Calibri 26", fg="green")
